616147b04ab89cd7556e954b194b84ad6f9a55fd
MrRevalis <dominik.revalis@gmail.com>
2020-08-11 14:13:34 +0200

    changes

---
+++ Clicker/Converters/BooleanToVisibility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Clicker
+{
+    public class BooleanToVisibility : BaseValueConverter<BooleanToVisibility>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null)
+            {
+                if ((bool)value)
+                    return Visibility.Hidden;
+                else
+                    return Visibility.Visible;
+            }
+            else
+            {
+                if ((bool)value)
+                    return Visibility.Visible;
+                else
+                    return Visibility.Hidden;
+            }
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
