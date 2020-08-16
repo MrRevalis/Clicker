8c85bb32943b2e2018554a386c3666cee18e3bad
MrRevalis <dominik.revalis@gmail.com>
2020-08-16 15:36:45 +0200

    xD

--- Clicker/obj/Debug/Clicker_Content.g.i.cs
+++ Clicker/obj/Debug/Clicker_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/clickericon.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/load.png")]
 
 
